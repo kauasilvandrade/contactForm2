18a4763e60304cc13400912af2e5ea889edea0e4
Kauã Andrade <kauasilvaz@hotmail.com>
2026-04-07 11:59:11 -0300

    Botão do formulário terminado

--- src/components/Form/components/Button/index.tsx
+++ src/components/Form/components/Button/index.tsx
@@ -2,7 +2,7 @@ export function Button() {
   return (
     <button
       className="w-full py-4 text-base font-semibold tracking-widest
-        mt-4 rounded-full uppercase bg-white text-[#DB2B75]"
+        mt-4 rounded-full uppercase bg-white text-[#DB2B75] cursor-pointer"
     >
       Enviar
     </button>
