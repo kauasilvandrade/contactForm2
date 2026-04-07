7a4ff740c234a28ac5f1270810584acf193da8b8
Kauã Andrade <kauasilvaz@hotmail.com>
2026-04-07 10:01:02 -0300

    Header finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,35 @@ import { Phone } from "lucide-react";
 import { MapPin } from "lucide-react";
 import { Speech } from "lucide-react";
 import { Mail } from "lucide-react";
+import { Header } from "./components/Header";
 
 export function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <Card
-        icon={<MapPin size={70} strokeWidth={1} color="#EE1C6F" />}
+    <div>
+      <Header />
+      {/* <Card
+        icon={<MapPin size={70} strokeWidth={1} color="#742499" />}
         title="Nosso escritório principal"
         description="SoHo 94 Broadway St New York, NY 1001"
       />
 
       <Card
-        icon={<Phone size={70} strokeWidth={1} color="#EE1C6F" />}
+        icon={<Phone size={70} strokeWidth={1} color="#742499" />}
         title="Número de telefone"
         description="234-9876-5400 888-0123-4567 (ligação gratuita)"
       />
 
       <Card
-        icon={<Speech size={70} strokeWidth={1} color="#EE1C6F" />}
+        icon={<Speech size={70} strokeWidth={1} color="#742499" />}
         title="fax"
         description="1-234-567-8900"
       />
 
       <Card
-        icon={<Mail size={70} strokeWidth={1} color="#EE1C6F" />}
+        icon={<Mail size={70} strokeWidth={1} color="#742499" />}
         title="E-mail"
         description="hello@theme.com"
-      />
+      /> */}
     </div>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,77 @@
+import { MenuIcon, XIcon } from "lucide-react";
+import Logo from "./../../assets/logo.png";
+import { useState } from "react";
+
+export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setMenuOpen((prev) => !prev);
+  }
+
+  function closeMenu() {
+    setMenuOpen(false);
+  }
+
+  return (
+    <header className="w-screen bg-white flex justify-center py-4 relative">
+      <div className="w-[340px] flex justify-between">
+        <a href="#">
+          <img src={Logo} alt="Logo" className="h-8" />
+        </a>
+
+        <button onClick={toggleMenu} className="cursor-pointer">
+          <MenuIcon size={30} />
+        </button>
+      </div>
+
+      {/* Overlay */}
+      {menuOpen && (
+        <div
+          className="fixed inset-0 bg-red-800 z-10 cursor-pointer"
+          onClick={closeMenu}
+        />
+      )}
+
+      {/* Menu */}
+      <nav
+        className={`fixed h-screen w-[250px] py-5 bg-[#000000db] top-0 z-20 transition-all duration-300 ${
+          menuOpen ? "left-0" : "-left-[250px]"
+        }`}
+      >
+        <button
+          className="px-2 absolute right-0 cursor-pointer"
+          onClick={closeMenu}
+        >
+          <XIcon color="white" size={35} />
+        </button>
+
+        <ul className="flex flex-col mt-9">
+          <li>
+            <a className="block p-5 text-white text-center text-[18px] hover:text-gray-300 cursor-pointer">
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a className="block p-5 text-white text-center text-[18px] hover:text-gray-300 cursor-pointer">
+              About Us
+            </a>
+          </li>
+
+          <li>
+            <a className="block p-5 text-white text-center text-[18px] hover:text-gray-300 cursor-pointer">
+              Contact
+            </a>
+          </li>
+
+          <li>
+            <a className="block p-5 text-white text-center text-[18px] hover:text-gray-300 cursor-pointer">
+              Community
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
